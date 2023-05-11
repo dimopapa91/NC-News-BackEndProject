38d16b4f9c27174669329b117f208e3783a6bae5
dimopapa91 <64607400+dimopapa91@users.noreply.github.com>
2023-05-11 10:55:41 +0100

    completed ticket_5

--- models/topics.models.js
+++ models/topics.models.js
@@ -25,3 +25,26 @@ exports.fetchApi = () => {
       });
   };
 
+  exports.fetchArticles = () => {
+    return connection.query(`
+    SELECT
+    articles.author,
+    articles.title,
+    articles.article_id,
+    articles.topic,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+
+    COUNT(*)::INT AS comment_count
+    FROM articles
+    
+    JOIN comments ON comments.article_id = articles.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;
+    `)
+    .then((result) => {
+        return result.rows;
+    });
+  };
+
