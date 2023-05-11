f7b9a21d41152185a1573d47a10be623ee9d3481
dimopapa91 <64607400+dimopapa91@users.noreply.github.com>
2023-05-11 10:21:59 +0100

    updated task_4 test as per feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -51,16 +51,17 @@ describe('/api/articles/:article_id', () => {
         .get('/api/articles/1')
         .expect(200)
         .then((res) => {
-            console.log(res.body);
-            expect(res.body.article).toHaveProperty('author');
-            expect(res.body.article).toHaveProperty('title');
-            expect(res.body.article).toHaveProperty('article_id');
-            expect(res.body.article).toHaveProperty('body');
-            expect(res.body.article).toHaveProperty('topic');
-            expect(res.body.article).toHaveProperty('created_at');
-            expect(res.body.article).toHaveProperty('votes');
-            expect(res.body.article).toHaveProperty('article_img_url');
-        })
+            expect(res.body.article).toEqual({
+                article_id: 1,
+                title: 'Living in the shadow of a great man',
+                topic: 'mitch',
+                author: 'butter_bridge',
+                body: 'I find this existence challenging',
+                created_at: '2020-07-09T20:11:00.000Z',
+                votes: 100,
+                article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+              });
+        });
     });
     test('GET - status: 404 - Should return an error when article not found', () => {
         return request(app)
